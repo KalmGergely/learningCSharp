29ab917271fc12372005e16d17d35fe40d77171c
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-27 20:21:45 +0100

    add simple project for the basics

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] testMatrix = CreateMatrix(4);
+            PrintMatrix(testMatrix);
+
+            Console.ReadKey();
+        }
+
+        static int[,] CreateMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 1;
+                    } else
+                    {
+                        matrix[i, j] = 0;
+                    }
+
+                }
+            }
+
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string line = "";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    line += matrix[i, j].ToString();
+                }
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
