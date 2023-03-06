f220afa2a5fd81edc69e5a8e25209029e7be26c5
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-06 06:26:29 +0100

    create isSymmetric trial exercise

---
+++ trial_exercises/isSymmetric/isSymmetric/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isSymmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] trueInput =
+            {
+                new int[]{1,0,1},
+                new int[]{0,2,2},
+                new int[]{1,2,5}
+            };
+
+            int[][] falseInput =
+            {
+                new int[]{1,1,1},
+                new int[]{0,2,2},
+                new int[]{1,2,5}
+            };
+
+            Console.WriteLine(isSymmetric(trueInput));
+            Console.WriteLine(isSymmetric(falseInput));
+            Console.ReadKey();
+        }
+
+        static Boolean isSymmetric(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] != array[j][i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
