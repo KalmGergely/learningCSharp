94c571b591909c0788610d34ca9d9a7badd8edfe
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-08 10:39:43 +0100

    create simple rest app

---
+++ HelloWorldApp/HelloWorldApp/Controllers/GreetingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloWorldApp.Models;
+
+namespace HelloWorldApp.Controllers
+{
+    [Route("api")]
+    public class GreetingController : Controller
+    {
+        [HttpGet("greeting")]
+        public ActionResult Greeting()
+        {
+            Greeting response = new Greeting() { Id = 1, Content = "Hello World!" };
+
+            return Ok(response);
+        }
+        
+        [HttpGet("greeting/{name}")]
+        public ActionResult GreetByName(string name) 
+        {
+            Greeting response = new Greeting() { Id = 1, Content = $"Hello {name}" };
+
+            return Ok(response);
+        }
+
+        [HttpGet("greetspec/{name}/")]
+        public ActionResult GreetSpecifically(string name, [FromQuery(Name = "greeting")] string greeting)
+        {
+            Greeting response = new Greeting() { Id = 1, Content = $"{greeting} {name}" };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ HelloWorldApp/HelloWorldApp/Models/Greeting.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWorldApp.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; } = null!;
+    }
+}
